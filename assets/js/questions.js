e2d69bf3b8aa50584e6d7b981d37f742050b95dd
sohail <as19adj@herts.ac.uk>
2024-01-16 17:11:30 +0000

    adds questions to question js file

---
+++ assets/js/questions.js
@@ -0,0 +1,21 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>","<js>","<scripting>","<script>"],
+        answer: "<script>"
+
+    },
+    {
+        title: 'Where is the correct place to insert a JavaScript?',
+        choices: ["The <head> section","The <body> section","The <footer> section","None of them"],
+        answer: "The <body> section"
+
+    },
+    {
+        title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        choices: ['<script href="xxx.js"','<script src="xxx.js"','<script name = "xxx.js"','script url="xxx.js"'],
+        answer: '<script src="xxx.js"'
+
+    }
+
+];
